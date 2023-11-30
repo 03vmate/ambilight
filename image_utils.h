4287e26bf292382ef5a29276f790b3b6d4508abe
03vmate <47510515+03vmate@users.noreply.github.com>
2023-11-30 04:33:34 +0100

    Performance optimizations

--- image_utils.h
+++ image_utils.h
@@ -1,52 +1,27 @@
 #include <stdint.h>
 #include <stdio.h>
 
-uint8_t*** flatbufToImg(void* buf, int width, int height) {
-    if(buf == nullptr) {
-        return nullptr;
-    }
-
-    uint8_t*** img = new uint8_t**[width];
-    for(int i = 0; i < width; i++) {
-        img[i] = new uint8_t*[height];
-        for(int j = 0; j < height; j++) {
-            img[i][j] = new uint8_t[3];
-            for(int k = 0; k < 3; k++) {
-                int index = j*width*3 + i*3 + k;
-                img[i][j][k] = ((uint8_t*)buf)[index];
-            }
-        }
-    }
-
-    return img;
-}
-
-void freeImg(uint8_t*** img, int width, int height) {
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
-            delete[] img[i][j];
-        }
-        delete[] img[i];
-    }
-    delete[] img;
-}
-
-uint8_t* colorOfBlock(uint8_t*** img, int x, int y, int width, int height) {
+uint8_t* colorOfBlock(const uint8_t* img, int imgwidth, int imgheight, int x, int y, int width, int height) {
     uint32_t* color = new uint32_t[3];
     color[0] = 0;
     color[1] = 0;
     color[2] = 0;
-    for(int xpos = x; xpos < x + width; xpos++) {
-        for(int ypos = y; ypos < y + height; ypos++) {
-            for(int i = 0; i < 3; i++) {
-                color[i] += img[xpos][ypos][i];
+
+    for (int xpos = x; xpos < x + width; xpos++) {
+        for (int ypos = y; ypos < y + height; ypos++) {
+            for (int i = 0; i < 3; i++) {
+                int index = (ypos * imgwidth + xpos) * 3 + i;
+                color[i] += img[index];
             }
         }
     }
+
     uint8_t* color_byte = new uint8_t[3];
-    for(int i = 0; i < 3; i++) {
+
+    for (int i = 0; i < 3; i++) {
         color_byte[i] = color[i] / (width * height);
     }
+
     delete[] color;
     return color_byte;
 }
--- main.cpp
+++ main.cpp
@@ -9,6 +9,14 @@
 #include "simpleConfigParser.h"
 #include "image_utils.h"
 #include "SerialPort.hpp"
+#include <csignal>
+
+bool run = true;
+
+struct buffer {
+    void* start;
+    size_t length;
+};
 
 void printV4L2Capability(const struct v4l2_capability& cap) {
     printf("Driver: %s\n", cap.driver);
@@ -17,7 +25,12 @@ void printV4L2Capability(const struct v4l2_capability& cap) {
     printf("Version: %u.%u.%u\n\n", (cap.version >> 16) & 0xFF, (cap.version >> 8) & 0xFF, cap.version & 0xFF);
 }
 
+void sighandler(int signum) {
+    run = false;
+}
+
 int main(int argc, char** argv) {
+    signal(SIGINT, sighandler);
     if (argc != 2) {
         std::cout << "Usage: " << argv[0] << " <config file>" << std::endl;
         return -1;
@@ -43,6 +56,7 @@ int main(int argc, char** argv) {
     float row_block_height = border_size;
     float row_block_width = (float)capture_width / horizontal_leds;
     double gamma = std::stod(config["gamma_correction"]);
+    int buffer_count = std::stoi(config["v4l2_buffer_count"]);
 
     int fd = open(config["capture_device"].c_str(), O_RDWR);
     if (fd == -1) {
@@ -86,27 +100,37 @@ int main(int argc, char** argv) {
     struct v4l2_requestbuffers req;
     req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     req.memory = V4L2_MEMORY_MMAP;
-    req.count = 1;
+    req.count = 4;
     if(ioctl(fd, VIDIOC_REQBUFS, &req) == -1) {
         std::cout << "Error requesting buffer" << std::endl;
         return -1;
     }
 
     // Map buffer
-    struct v4l2_buffer buf;
-    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    buf.memory = V4L2_MEMORY_MMAP;
-    buf.index = 0;
-    if(ioctl(fd, VIDIOC_QUERYBUF, &buf) == -1) {
-        std::cout << "Error querying buffer" << std::endl;
-        return -1;
+    buffer buffers[buffer_count];
+    for(int i = 0; i < buffer_count; i++) {
+        struct v4l2_buffer buf;
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = i;
+        if(ioctl(fd, VIDIOC_QUERYBUF, &buf) == -1) {
+            std::cout << "Error querying buffer" << std::endl;
+            return -1;
+        }
+        buffers[i].length = buf.length;
+        buffers[i].start = mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
     }
-    void *buffer = mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
 
     // Queue buffer
-    if(ioctl(fd, VIDIOC_QBUF, &buf) == -1) {
-        std::cout << "Error queuing buffer" << std::endl;
-        return -1;
+    for(int i = 0; i < buffer_count; i++) {
+        struct v4l2_buffer buf;
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = i;
+        if(ioctl(fd, VIDIOC_QBUF, &buf) == -1) {
+            std::cout << "Error queueing buffer" << std::endl;
+            return -1;
+        }
     }
 
     // Start streaming
@@ -119,18 +143,30 @@ int main(int argc, char** argv) {
     // Create JPEG decompressor
     tjhandle tjhandle = tjInitDecompress();
 
+    // Buffer for holding the decoded rgb data
     unsigned char *rgbBuffer = nullptr;
 
-    while (true) {
+    // Buffer for holding the LED rgb data to send to MCU
+    uint8_t* leddata = new uint8_t[(horizontal_leds + vertical_leds) * 2 * 3];
+
+    while (run) {
+        auto start = std::chrono::high_resolution_clock::now();
+
         // Dequeue buffer
+        v4l2_buffer buf = {};
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+
         if (ioctl(fd, VIDIOC_DQBUF, &buf) == -1) {
-            std::cout << "Error dequeuing buffer" << std::endl;
+            std::cout << "Error dequeueing buffer" << std::endl;
             return -1;
         }
 
-        // Decompress JPEG
+        auto dqtime = std::chrono::high_resolution_clock::now();
+
+
         int width, height, jpegsubsamp, jpegcolorspace;
-        int header_result = tjDecompressHeader3(tjhandle, static_cast<unsigned char*>(buffer), buf.bytesused, &width, &height, &jpegsubsamp, &jpegcolorspace);
+        int header_result = tjDecompressHeader3(tjhandle, static_cast<unsigned char*>(buffers[buf.index].start), buffers[buf.index].length, &width, &height, &jpegsubsamp, &jpegcolorspace);
         if(header_result == -1) {
             std::cout << "Error decompressing header, fetching new buffer" << std::endl;
             // Queue buffer
@@ -146,18 +182,18 @@ int main(int argc, char** argv) {
         }
 
         // Decompress jpeg
-        tjDecompress2(tjhandle, static_cast<unsigned char*>(buffer), buf.bytesused, rgbBuffer, width, 0, height, TJPF_RGB, 0);
+        tjDecompress2(tjhandle, static_cast<unsigned char*>(buffers[buf.index].start), buffers[buf.index].length, rgbBuffer, width, 0, height, TJPF_RGB, 0);
 
-        uint8_t*** img = flatbufToImg(rgbBuffer, width, height);
-        uint8_t* leddata = new uint8_t[(horizontal_leds + vertical_leds) * 2 * 3];
+        auto decomptime = std::chrono::high_resolution_clock::now();
+
+
         ssize_t leddata_index = 0;
-
         
         //right column, bottom to top
         for(int i = vertical_leds - 1; i >= 0; i--) {
             int block_top = i * column_block_height;
-            int block_left = 0;
-            uint8_t* color = colorOfBlock(img, block_left, block_top, column_block_width, column_block_height);
+            int block_left = capture_width - column_block_width;
+            uint8_t* color = colorOfBlock(rgbBuffer, width, height, block_left, block_top, column_block_width, column_block_height);
             leddata[leddata_index++] = color[0];
             leddata[leddata_index++] = color[1];
             leddata[leddata_index++] = color[2];
@@ -168,7 +204,7 @@ int main(int argc, char** argv) {
         for(int i = horizontal_leds - 1; i >= 0; i--) {
             int block_top = 0;
             int block_left = i * row_block_width;
-            uint8_t* color = colorOfBlock(img, block_left, block_top, row_block_width, row_block_height);
+            uint8_t* color = colorOfBlock(rgbBuffer, width, height, block_left, block_top, row_block_width, row_block_height);
             leddata[leddata_index++] = color[0];
             leddata[leddata_index++] = color[1];
             leddata[leddata_index++] = color[2];
@@ -179,7 +215,7 @@ int main(int argc, char** argv) {
         for(int i = 0; i < vertical_leds; i++) {
             int block_top = i * column_block_height;
             int block_left = 0;
-            uint8_t* color = colorOfBlock(img, block_left, block_top, column_block_width, column_block_height);
+            uint8_t* color = colorOfBlock(rgbBuffer, width, height, block_left, block_top, column_block_width, column_block_height);
             leddata[leddata_index++] = color[0];
             leddata[leddata_index++] = color[1];
             leddata[leddata_index++] = color[2];
@@ -190,7 +226,7 @@ int main(int argc, char** argv) {
         for(int i = 0; i < horizontal_leds; i++) {
             int block_top = capture_height - row_block_height;
             int block_left = i * row_block_width;
-            uint8_t* color = colorOfBlock(img, block_left, block_top, row_block_width, row_block_height);
+            uint8_t* color = colorOfBlock(rgbBuffer, width, height, block_left, block_top, row_block_width, row_block_height);
             leddata[leddata_index++] = color[0];
             leddata[leddata_index++] = color[1];
             leddata[leddata_index++] = color[2];
@@ -205,19 +241,34 @@ int main(int argc, char** argv) {
             }
         }
 
+        auto proctime = std::chrono::high_resolution_clock::now();
+
+
         // Send data
         mcu.write(reinterpret_cast<const char*>(leddata), (horizontal_leds + vertical_leds) * 2 * 3);
         mcu.write("\n", 1);
 
-        std::cout << "Update" << std::endl;
+        auto writetime = std::chrono::high_resolution_clock::now();
 
-        freeImg(img, width, height);
 
         // Queue buffer
         while (ioctl(fd, VIDIOC_QBUF, &buf) == -1) {
             std::cout << "Error queuing buffer" << std::endl;
         }
+
+        auto stop = std::chrono::high_resolution_clock::now();
+
+        auto dqduration = std::chrono::duration_cast<std::chrono::milliseconds>(dqtime - start);
+        auto decompduration = std::chrono::duration_cast<std::chrono::milliseconds>(decomptime - dqtime);
+        auto procduration = std::chrono::duration_cast<std::chrono::milliseconds>(proctime - decomptime);
+        auto writeduration = std::chrono::duration_cast<std::chrono::milliseconds>(writetime - proctime);
+        auto queueduration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - writetime);
+        auto totalduration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+        std::cout << "\rdq: " << dqduration.count() << "ms | decomp: " << decompduration.count() << "ms | proc: " << procduration.count() << "ms | write: " << writeduration.count() << "ms | queue: " << queueduration.count() << "ms | total: " << totalduration.count() << "ms";
+        std::cout.flush();
     }
+
+    std::cout << "Stopping" << std::endl;
 
     delete[] rgbBuffer;
 
@@ -228,7 +279,9 @@ int main(int argc, char** argv) {
     }
 
     // Unmap buffer
-    munmap(buffer, buf.length);
+    for (int i = 0; i < buffer_count; ++i) {
+        munmap(buffers[i].start, buffers[i].length);
+    }
 
     // Close device
     close(fd);
