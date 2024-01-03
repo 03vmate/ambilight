d3b6f5e05b7472d682221b016d4f6da2ab6e5788
03vmate <47510515+03vmate@users.noreply.github.com>
2024-01-03 19:24:10 +0100

    Extract colorOfBlock from v4l2mode.cpp

---
+++ colorOfBlock.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <cstdint>
+
+uint8_t* colorOfBlock(const uint8_t* img, int imgwidth, int imgheight, int x, int y, int width, int height);
--- v4l2mode.hpp
+++ v4l2mode.hpp
@@ -4,6 +4,5 @@
 #include <map>
 
 void v4l2_sighandler(int signum);
-uint8_t* colorOfBlock(const uint8_t* img, int imgwidth, int imgheight, int x, int y, int width, int height);
 uint8_t gammaCorrection(uint8_t inputBrightness, double gamma);
 void start_v4l2mode(std::map<std::string, std::string> config);
